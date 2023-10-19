637a429979673ec73bf4cf5cf3decf6097495a5f
Trinh Nguyen Hoang Vu <trinhnguyenhoangvu306@gmail.com>
2023-10-19 23:01:47 +0700

    final fixed

--- Bài tập thực hành CTDL&GT tuần 5.cpp
+++ Bài tập thực hành CTDL&GT tuần 5.cpp
@@ -1,287 +1,276 @@
-/*
-Thông tin của một quyển sách trong thư viện gồm các thông tin: 
-- Tên sách (chuỗi) 
-- Tác giả (chuỗi, tối đa 5 tác giả) 
-- Nhà xuất bản (chuỗi)
-- Năm xuất bản (số nguyên)
-1.  Hãy tạo danh sách liên kết (đơn hoặc kép) chứa thông tin các quyển sách có trong thư viện (được nhập từ bàn phím). 
-    Ghi danh sách liên kết vào File
-2.  Cho biết số lượng các quyển sách của một tác giả bất kỳ (nhập từ bàn phím). 
-3.  Trong năm YYYY (nhập từ bàn phím), nhà xuất bản ABC (nhập từ bàn phím) đã phát hành những quyển sách nào. 
-4.  Xóa tất cả các quyển sách của tác giả X (X: tham số của hàm). Ghi kết quả sách liên kết vào File (khác File trong câu 1)
-5.  Thống kê số lượng sách của mỗi tác giả và ghi kết quả thống kê lên tệp. 
-6.  Đọc dữ liệu thống kê trong câu 5. Xuất lên màn hình.
-7.  Đọc dữ liệu từ tệp trong câu a và đưa vào danh sách liên kết đơn.
-8.  Đọc dữ liệu thống kê trong câu 5 và đưa vào một danh sách mới có lưu trữ thông tin gồm: tên tác giả và số lượng sách
-*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 const int maxn = 100;
 
+// define struct
 struct Book {
-    char name[30];
-    char Author[5][50];
-    char nhaXuatBan[30];
+    char name[50];
+    char authors[5][50];
+    char producer[50];
     int numberOfAuthor;
     int publishingYear;
 };
-struct Node {
-    Book data;
-    Node *pNext;
-};
-struct ThuVien {
-    Node *pHead;
-    Node *pTail;
-    int size;
-};
 struct Author {
-    char Author[30];
-    int soLuongSach;
+    char name[50];
+    int numberOfBook;
+};
+struct NodeBook {
+    Book book;
+    NodeBook* pNext;
 };
 struct NodeAuthor {
-    Author data;
-    NodeAuthor *pNext;
+    Author author;
+    NodeAuthor* pNext;
 };
-struct ListAuthor {
-    NodeAuthor *Head;
-    NodeAuthor *Tail;
+struct Library {
+    NodeBook* pHead;
+    NodeBook* pTail;
+};
+struct AuthorList {
+    NodeAuthor* pHead;
+    NodeAuthor* pTail;
 };
 
-// Init
-void InitAuthor(Author &X) { X.soLuongSach = 0; }
-void InitListAuthor(ListAuthor &TG) { TG.Head = TG.Tail = NULL; }
-void InitList(ThuVien &L) { L.pHead = L.pTail = NULL, L.size = 0; }
-void Input(Book &A) {
-    fflush(stdin);
-    printf("Nhap ten sach : ");
-    gets(A.name);
+// init
+void initAuthorList(AuthorList &L) { L.pHead = L.pTail = NULL; }
+void initLibrary(Library &L) { L.pHead = L.pTail = NULL; }
+NodeBook* createNodeBook(Book &book) {
+    NodeBook* nodebook = new NodeBook;
+    if(nodebook == NULL) return NULL;
+
+    nodebook->book = book;
+    nodebook->pNext = NULL;
+    return nodebook;
+}
+void inputBook(Book &book) {
+    printf("Nhap ten sach: ");
+    gets(book.name);
     do {
-        printf("Nhap so luong tac gia : ");
-        scanf("%d", &A.numberOfAuthor);
-        if (A.numberOfAuthor > 5) printf("So luong tac gia khong hop le !!\n");
-    } while (A.numberOfAuthor > 5);
-    
-    fflush(stdin);
-    for (int i = 0; i < A.numberOfAuthor; ++i) {
-        printf("Nhap tac gia thu %d : ", i + 1);
-        gets(A.Author[i]);
+        printf("Nhap so luong tac gia: ");
+        scanf("%d", &book.numberOfAuthor);
+        if(book.numberOfAuthor <= 0 || book.numberOfAuthor > 5) 
+            printf("So luong tac gia must be in [0, 5]!!\n");
+    } while (book.numberOfAuthor <= 0 || book.numberOfAuthor > 5);
+
+    for(int i = 0; i < book.numberOfAuthor; i++) {
         fflush(stdin);
+        printf("Nhap ten tac gia thu %d: ", i + 1);
+        gets(book.authors[i]);
     }
-    
-    printf("Nhap ten nha xuat ban : ");
-    gets(A.nhaXuatBan);
-    printf("Nhap nam xuat ban sach : ");
-    scanf("%d", &A.publishingYear);
-    fflush(stdin);
-}
-Node *createNode(Book A) {
-    Node *p = new Node;
-    p->data = A;
-    p->pNext = NULL;
-    return p;
-}
 
-// add
-void add_Author(ListAuthor &TG, char *Author, int soLuongSach) {
-    NodeAuthor *p = new NodeAuthor;
-    if (p == NULL) return;
+    printf("Nhap ten nha san xuat: ");
+    gets(book.producer);
 
-    strcpy(p->data.Author, Author);
-    p->data.soLuongSach = soLuongSach;
-    p->pNext = NULL;
-    
-    if (TG.Head == NULL) TG.Head = TG.Tail = p;
-    else {
-        TG.Tail->pNext = p;
-        TG.Tail = p;
-    }
-}
-void add_Book(ThuVien &L, Book A) {
-    Node *p = createNode(A);
-    if (p == NULL) return;
-    
-    if (L.pHead == NULL) L.pHead = L.pTail = p;
-    else {
-        L.pTail->pNext = p;
-        L.pTail = p;
-    }
-}
-void create_ThuVien(ThuVien &L) {
-    printf("Nhap so luong sach : ");
-    scanf("%d", &L.size);
-    
-    Book temp;
-    for (int i = 0; i < L.size; ++i) {
-        Input(temp);
-        add_Book(L, temp);
-    }
+    printf("Nhap nam san xuat: "); 
+    scanf("%d", &book.publishingYear);
 }
 
 // display
-void Output(Book A) {
-    printf("Ten sach : %s\n", A.name);
-    printf("Danh sach tac gia \n");
-    for (int i = 0; i < A.numberOfAuthor; ++i) printf("Tac gia thu %d : %s\n", i + 1, A.Author[i]);
-    printf("Nha xuat ban : %s\n", A.nhaXuatBan);
-    printf("Nam xuat ban : %d\n", A.publishingYear);
+void display_Book(Book book) {
+    printf("Ten sach: %s\n", book.name);
+    printf("Danh sach tac gia:\n");
+    for(int i = 0; i < book.numberOfAuthor; i++) 
+        printf("\t%d. %s\n", i + 1, book.authors[i]);
+    printf("Nha san xuat: %s\n", book.producer);
+    printf("Nam san xuat: %d\n", book.publishingYear);
 }
-void display(ThuVien L) {
-    Node *p = L.pHead;
+void display_Library(Library L) {
+    NodeBook* p = L.pHead;
     int i = 0;
-    while (p != NULL) {
-        printf("----Thong Tin Sach Thu %d------\n", ++i);
-        Output(p->data);
+    while(p != NULL) {
+        printf("----Thong tin danh sach thu %d----\n", ++i);
+        display_Book(p->book);
         p = p->pNext;
     }
 }
-void display2(ListAuthor TG) {
-    NodeAuthor *p = TG.Head;
-    while (p != NULL) {
-        printf("Ten tac gia : %s\n", p->data.Author);
-        printf("So luong Sach : %d\n", p->data.soLuongSach);
-        p = p->pNext;
+void display_AuthorList(AuthorList L) {
+    NodeAuthor* nodeauthor = L.pHead;
+    while(nodeauthor != NULL) {
+        printf("Ten tac gia: %s\n", nodeauthor->author.name);
+        printf("So luong sach: %d\n", nodeauthor->author.numberOfBook);
+        nodeauthor = nodeauthor->pNext;
+    }
+}
+
+// add
+void addBook(Library &L, Book book) {
+    NodeBook* nodebook = createNodeBook(book);
+    if(nodebook == NULL) return;
+
+    if(L.pHead == NULL) L.pHead = L.pTail = nodebook;
+    else {
+        L.pTail->pNext = nodebook;
+        L.pTail = nodebook;
+    }
+}
+void addAuthor(AuthorList &L, char* author, int numberOfBook) {
+    NodeAuthor* nodeauthor = new NodeAuthor;
+    if(nodeauthor == NULL) return;
+
+    strcpy(nodeauthor->author.name, author);
+    nodeauthor->author.numberOfBook = numberOfBook;
+    nodeauthor->pNext = NULL;
+    
+    if(L.pHead == NULL) L.pHead = L.pTail = nodeauthor;
+    else {
+        L.pTail->pNext = nodeauthor;
+        L.pTail = nodeauthor;
+    }
+}
+void createLibrary(Library &L) {
+    initLibrary(L);
+    printf("Nhap so luong sach: ");
+    int n; scanf("%d", &n);
+
+    Book book;
+    for(int i = 0; i < n; i++) {
+        fflush(stdin);
+        inputBook(book);
+        addBook(L, book);
     }
 }
 
 // methods
-int findAuthor(ListAuthor &TG, char *name) {
-    NodeAuthor *p = TG.Head;
-    while (p != NULL) {
-        if (strcmp(p->data.Author, name) == 0) {
-            p->data.soLuongSach++;
+int numberOfBookByAuthor(Library L, char* author) {
+    int cnt = 0;
+    NodeBook* p = L.pHead;
+    while(p != NULL) {
+        for(int i = 0; i < p->book.numberOfAuthor; i++) 
+            if(strcmp(author, p->book.authors[i]) == 0)
+                ++cnt;
+        p = p->pNext;
+    }
+    return cnt;
+}
+void Book_Producer_inYear(Library L, char* producer, int year) {
+    Book a[maxn];
+    int cnt = 0;
+    NodeBook* p = L.pHead;
+    while(p != NULL) {
+        if( strcmp(p->book.producer, producer) == 0 &&
+            year == p->book.publishingYear) a[cnt++] = p->book;
+        p = p->pNext;
+    }
+
+    if(cnt == 0) printf("Nha xuat ban %s khong xuat ban sach trong nam %d\n", producer, year);
+    else {
+        printf("---Danh sach nhung cuon sach nha xuat ban %s xuat ban trong nam %d---\n", producer, year);
+        for(int i = 0; i < cnt; i++) printf("%d. %s\n", i + 1, a[i].name);
+    }
+}
+int findAuthor(AuthorList L, char* author) {
+    NodeAuthor* p = L.pHead;
+    while(p != NULL) {
+        if(strcmp(p->author.name, author) == 0) {
+            p->author.numberOfBook++;
             return 1;
         }
         p = p->pNext;
     }
     return 0;
 }
-int count_SoSach_of_Tacgia(ThuVien TV, char *name) {
-    int cnt = 0;
-    Node *p = TV.pHead;
-    Book tmp;
-    while (p != NULL) {
-        tmp = p->data;
-        for (int i = 0; i < tmp.numberOfAuthor; ++i) if (!strcmp(name, tmp.Author[i])) ++cnt;
-        p = p->pNext;
-    }
-    return cnt;
-}
-void soSach_xuatBan_trongNam(ThuVien TV, char *tenNhaXB, int namXB) {
-    int check = 0, cnt = 0;
-    Book a[maxn];
-    Node *p = TV.pHead;
-    while (p != NULL) {
-        Book temp = p->data;
-        if (temp.publishingYear == namXB && strcmp(temp.nhaXuatBan,tenNhaXB) == 0) a[cnt++] = temp, check = 1;
-        p = p->pNext;
-    }
-    if (!check) printf("Nha xuat ban %s khong xuat ban sach nao trong nam %d\n", tenNhaXB, namXB);
-    else {
-        printf("---Danh sach nhung cuon sach nha xuat ban %s xuat ban trong nam %d---\n", tenNhaXB, namXB);
-        for (int i = 0; i < cnt; ++i) printf("%d. %s\n", i + 1, a[i].name);
+void statisticBook_Author(Library L, AuthorList &AL) {
+    initAuthorList(AL);
+    NodeBook* nodebook = L.pHead;
+    while(nodebook != NULL) {
+        for(int i = 0; i < nodebook->book.numberOfAuthor; i++)
+            if(findAuthor(AL, nodebook->book.authors[i]) == 0) 
+                addAuthor(AL, nodebook->book.authors[i], 1);
+        nodebook = nodebook->pNext;
     }
 }
-
-// file
-void read_Library_from_File(ThuVien &L, const char *fileName) {
-    InitList(L);
-    FILE *file = fopen(fileName, "rb");
-    if (file == NULL) {
-        printf("Khong mo duoc file!");
-        return;
-    }
-
-    Book temp;
-    while (fread(&temp, sizeof(Book), 1, file)) add_Book(L, temp);
-    
-    display(L);
-    fclose(file);
-    printf("\nDoc danh sach sach tu file thanh cong!\n");
-}
-void write_Library_to_File(ThuVien L, const char *fileName) {
-    FILE *file = fopen(fileName, "wb");
-    if (file == NULL) {
-        printf("Khong mo duoc file!");
-        return;
-    }
-
-    Node *p = L.pHead;
-    while (p != NULL) {
-        fwrite(&p->data, sizeof(Book), 1, file);
-        p = p->pNext;
-    }
-
-    fclose(file);
-    printf("\nGhi danh sach sach vao file thanh cong!\n");
-}
-void deleteBooks(ThuVien &TV, char *Author, const char *filename) {
-    Node *p = TV.pHead;
-    Node *prev = NULL;
-    while (p != NULL) {
+void deleteBooks(Library &L, char* author) {
+    NodeBook* p = L.pHead;
+    NodeBook* prev = NULL;
+    while(p != NULL) {
         int ok = 0;
-        for (int i = 0; i < p->data.numberOfAuthor; ++i) 
-            if (strcmp(Author, p->data.Author[i]) == 0) {
-                if (prev == NULL) TV.pHead = p->pNext;
+        for(int i = 0; i < p->book.numberOfAuthor; i++)
+            if(strcmp(author, p->book.authors[i]) == 0) {
+                if(prev == NULL) L.pHead = p->pNext;
                 else prev->pNext = p->pNext;
+
+                if(p == L.pTail) L.pTail = prev;
                 
-                if (p == TV.pTail) TV.pTail = prev;
-                
-                Node *temp = p;
+                NodeBook* t = p;
                 p = p->pNext;
-                free(temp);
+                free(t);
                 ok = 1;
-                
                 break;
             }
         
-        if (!ok) {
+        if(ok == 0) {
             prev = p;
             p = p->pNext;
         }
     }
-    write_Library_to_File(TV, filename);
 }
-void ThongKeSach(ThuVien TV, ListAuthor &TG, const char *filename) {
-    Node *p = TV.pHead;
-    InitListAuthor(TG);
-    while (p != NULL) {
-        for (int i = 0; i < p->data.numberOfAuthor; ++i)
-            if (!findAuthor(TG, p->data.Author[i]))
-                add_Author(TG, p->data.Author[i], 1);
-        
-        p = p->pNext;
+
+// file
+void writeLibraryToFile(Library L, const char* filename) {
+    FILE* file = fopen(filename, "wb");
+    if(file == NULL) {
+        printf("Khong mo duoc file!!!\n");
+        return;
     }
 
-    FILE *f = fopen(filename, "wb");
-    NodeAuthor *q = TG.Head;
-    while (q != NULL) {
-        fwrite(&q->data, sizeof(Author), 1, f);
-        q = q->pNext;
+    NodeBook* nodebook = L.pHead;
+    while(nodebook != NULL) {
+        fwrite(&nodebook->book, sizeof(Book), 1, file);
+        nodebook = nodebook->pNext;
+    }
+    printf("Ghi danh sach sach vao file thanh cong!\n");
+    
+    fclose(file);
+}
+void writeAuthorListToFile(AuthorList L, const char* filename) {
+    FILE* file = fopen(filename, "wb");
+    if(file == NULL) {
+        printf("Khong mo duoc file!!!\n");
+        return;
     }
 
-    fclose(f);
+    NodeAuthor* nodeauthor = L.pHead;
+    while(nodeauthor != NULL) {
+        fwrite(&nodeauthor->author, sizeof(Author), 1, file);
+        nodeauthor = nodeauthor->pNext;
+    }
+    printf("Ghi danh sach tac gia vao file thanh cong!\n");
+
+    fclose(file);
 }
-void readThongKe1(ListAuthor &tmp, const char *filename) {
-    FILE *f = fopen(filename, "rb");
-    InitListAuthor(tmp);
-    Author temp;
-    while (fread(&temp, sizeof(Author), 1, f)) add_Author(tmp, temp.Author, temp.soLuongSach);
-    
-    fclose(f);
+void readLibraryFromFile(Library &L, const char* filename) {
+    initLibrary(L);
+    FILE* file = fopen(filename, "rb");
+    if(file == NULL) {
+        printf("Khong mo duoc file!!!\n");
+        return;
+    }
+
+    Book book;
+    while(fread(&book, sizeof(Book), 1, file)) addBook(L, book);
+    display_Library(L);
+    printf("Doc danh sach sach tu file thanh cong!\n");
+
+    fclose(file);
 }
-void readThongKe2(ListAuthor &tmp, const char *filename) {
-    FILE *f = fopen(filename, "rb");
-    InitListAuthor(tmp);
-    Author temp;
-    while (fread(&temp, sizeof(Author), 1, f)) add_Author(tmp, temp.Author, temp.soLuongSach);
-    
-    display2(tmp);
-    fclose(f);
+void readAuthorListFromFile(AuthorList &L, const char* filename) {
+    initAuthorList(L);
+    FILE* file = fopen(filename, "rb");
+    if(file == NULL) {
+        printf("Khong mo duoc file!!!\n");
+        return;
+    }
+
+    Author author;
+    while(fread(&author, sizeof(Author), 1, file)) addAuthor(L, author.name, author.numberOfBook);
+    printf("Doc danh sach tac gia thanh cong!!!\n");
+
+    fclose(file);
 }
 
-void Menu() {
+void menu() {
     printf("----- MOI LUA CHON CHUC NANG -----\n");
     printf("1. Tao ThuVien Sach va ghi vao File\n");
     printf("2. Cho biet so luong sach cua mot tac gia bat ki\n");
@@ -292,65 +281,73 @@ void Menu() {
     printf("7. Doc du lieu tu tep trong cau 1 va dua vao danh sach lien ket don\n");
     printf("8. Doc du lieu thong ke tu cau 5 va dua vao mot danh sach moi\n");
     printf("9. Doc du lieu tu tep trong cau 4\n");
-    printf("10. Thoat chuong trinh\n");
+    printf("10. Lam sach giao dien\n");
+    printf("11. Thoat chuong trinh\n");
 }
 
 int main() {
-    ThuVien library1, library2, library3;
-    ListAuthor TG, temp1, temp2;
-    InitListAuthor(TG), InitListAuthor(temp1), InitListAuthor(temp2),
-    InitList(library1), InitList(library2), InitList(library3);
-    
-    char filename1[] = "library1.bin"; // file luu yeu cau 1
-    char filename2[] = "library2.bin"; // file luu yeu cau 4
-    char filename3[] = "Author.bin"; // file luu yeu cau 5
-    char authorName[50], nhaXuatBan[50];
+    Library Lib1, Lib2, Lib3;
+    AuthorList auList1, auList2, auList3;
 
-    int running = 1, choose, count1, year;
-    Menu();
-    while (running) {
-        printf("Moi nhap lua chon : ");
-        scanf("%d", &choose);
-        switch (choose) {
+    char filename1[] = "library1.txt";
+    char filename2[] = "library2.txt";
+    char filename3[] = "author.txt";
+    char author[50];
+
+    int running = 1, choice, count, year;
+    menu();
+    while(running) {
+        printf("Nhap lua chon: ");
+        scanf("%d", &choice);
+        switch(choice) {
             case 1:
-                create_ThuVien(library1);
-                write_Library_to_File(library1, filename1);
+                createLibrary(Lib1);
+                writeLibraryToFile(Lib1, filename1);
                 break;
             case 2:
-                printf("Nhap ten tac gia can kiem tra : ");
                 fflush(stdin);
-                gets(authorName);
-                count1 = count_SoSach_of_Tacgia(library1, authorName);
-                printf("So luong sach cua tac gia %s : %d\n", authorName, count1);
+                printf("Nhap ten tac gia: ");
+                gets(author);
+                count = numberOfBookByAuthor(Lib1, author);
+                printf("So luong sach cua tac gia %s: %d\n", author, count);
                 break;
             case 3:
-                printf("Nhap ten nha xuat ban can kiem tra : ");
                 fflush(stdin);
-                gets(nhaXuatBan);
-                printf("Nhap nam xuat ban : ");
+                printf("Nhap ten nha san xuat: ");
+                gets(author);
+
+                fflush(stdin);
+                printf("Nhap nam xuat ban: ");
                 scanf("%d", &year);
-                soSach_xuatBan_trongNam(library1, nhaXuatBan, year);
+                Book_Producer_inYear(Lib1, author, year);
                 break;
             case 4:
-                printf("Nhap ten tac gia cua sach can xoa : ");
                 fflush(stdin);
-                gets(authorName);
-                deleteBooks(library1, authorName, filename2);
+                printf("Nhap ten tac gia muon xoa: ");
+                gets(author);
+                deleteBooks(Lib1, author);
+                writeLibraryToFile(Lib1, filename2);
                 break;
             case 5:
-                ThongKeSach(library1, TG, filename3);
+                statisticBook_Author(Lib1, auList1);
+                writeAuthorListToFile(auList1, filename3);
                 break;
             case 6:
-                readThongKe2(temp1, filename3);
+                readAuthorListFromFile(auList2, filename3);
+                display_AuthorList(auList2);
                 break;
             case 7:
-                read_Library_from_File(library2, filename1);
+                readLibraryFromFile(Lib2, filename1);
                 break;
             case 8:
-                readThongKe1(temp2, filename3);
+                readAuthorListFromFile(auList3, filename3);
                 break;
             case 9:
-                read_Library_from_File(library3, filename2);
+                readLibraryFromFile(Lib3, filename2);
+                break;
+            case 10:
+                system("cls");
+                menu();
                 break;
             default:
                 running = 0;
